e4fff313b38c4c75a9ce50d4dc8b0f8e0e97859c
Haerang94 <47172557+Haerang94@users.noreply.github.com>
2019-04-10 11:48:03 +0900

    Update 백준10828번 스택stl.cpp

--- 백준10828번 스택stl.cpp
+++ 백준10828번 스택stl.cpp
@@ -1,60 +1,42 @@
 #include <iostream>
-#include<string>
+#include <string>
+#include<stack>
 using namespace std;
-
-typedef struct Stack{
-	int data[10000];
-	int size;
-	Stack() {
-		size = 0;
-	}
-	bool empty() {
-		return (size == 0);
-	}
-	void push(int item){
-		data[++size] = item;
-	}
-	void pop() {
-		if (empty())cout << -1 << endl;
-		else {
-			cout << data[size] << endl;
-			size--;
-		}
-	}
-
-
-	void top() {
-		if (empty())cout << -1 << endl;
-		cout << data[size] << endl;
-	}
-
-
-}Stack;
 
 int main() {
 	int n;
-	string cmd;
-	Stack s;
 	cin >> n;
+	stack<int> s;
 	while (n--) {
+		string cmd;
 		cin >> cmd;
 		if (cmd == "push") {
-			int n;
-			cin >> n;
-			s.push(n);
+			int k;
+			cin >> k;
+			s.push(k);
 		}
 		else if (cmd == "pop") {
-			s.pop();
-		}
-		else if (cmd == "top") {
-			s.top();
+			if (s.empty())cout << -1 << endl;
+			else {
+				cout << s.top() << endl;
+				s.pop();
+			}
+
 		}
 		else if (cmd == "size") {
-			cout << s.size << endl;
+			cout << s.size() << endl;
 		}
 		else if (cmd == "empty") {
 			cout << s.empty() << endl;
 		}
+		else if (cmd == "top") {
+			if (s.empty())cout << -1 << endl;
+			else cout << s.top() << endl;
+		}
+
+
 	}
 
+
+	return 0;
 }
